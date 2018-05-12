5af42b671cb09406fc49391ff8fd0b657c760ac5
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-12 09:51:22 +0300

    Calculate ending vowel sound frequencies

--- word-analyser/ending_vowel_frequencies.py
+++ word-analyser/ending_vowel_frequencies.py
@@ -4,26 +4,30 @@
 import sys
 import tools
 import re
+import character_analysis_tools as analysis
 
-vowels = u"aeiouyäö"
+# The vowel harmony rule states that vowels in the left and right groups can
+# never be in the same word. It has exceptions, but I don't think they apply at
+# the very end of the word.
+left   = u"äöy"
+right  = u"aou"
+middle = u"ei"
+
+def pairs(vowels):
+    return tools.all_character_pairs(vowels, length = 2)
 
 # ija is a special case. It's expressed via the -IA stroke.
-vowel_permutations = tools.all_character_pairs(vowels, length = 2) + ["ija"]
+vowel_permutations = pairs(left + middle) + pairs(right + middle) + [u"ija"] + [u"ijä"]
 
 def has_ending_vowels(word):
-    re.search()
+    for vowel_permutation in vowel_permutations:
+        regex = ".*" + vowel_permutation + "$"
+        if re.match(regex, word) is not None:
+            return True
 
 def main():
-    words = tools.get_finnish_wordlist()
-
-    results = map(
-        lambda c: compactify(
-            tools.permutations_present_in_words(c, words, has_ending_vowels)),
-        vowel_permutations)
-
-    results = filter(has_matches, results)
-    results = sorted(results, key = count_of_words, reverse = True)
-
+    results = analysis.frequency_analysis(vowel_permutations,
+                                          word_filter = has_ending_vowels)
     tools.save_results_into_file(results, "results/ending_vowel_frequencies.yaml")
 
     return 0 # no error
