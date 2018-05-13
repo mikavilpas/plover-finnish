04676b6cbaa23b0e8705a11e2da0b6ae944f9b1b
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-13 16:28:47 +0300

    Fix bugs in double consonant cluster calculation

--- word-analyser/word_analyser/double_consonant_frequencies.py
+++ word-analyser/word_analyser/double_consonant_frequencies.py
@@ -10,7 +10,7 @@ consonants = "hklmnprst"
 
 def consonant(character) -> bool:
     all_consonants = "bcdfghjklmnpqrstvwxz"
-    character in all_consonants
+    return character in all_consonants
 
 def find_neighboring_characters_in_word(word, chars):
     if len(word) == len(chars):
@@ -22,32 +22,31 @@ def find_neighboring_characters_in_word(word, chars):
         return None # doesn't contain chars
 
     preceding_character = word[index - 1] if not word.startswith(chars) else Nothing
-    trailing_character = word[index - 1 + len(chars) - 1] if not word.endswith(chars) else Nothing
+    trailing_character = word[index - 1 + len(chars) + 1] if not word.endswith(chars) else Nothing
 
     return dict(preceding_character = preceding_character,
                 trailing_character = trailing_character)
 
-def after_consonant(word, chars) -> bool:
+def after_vowel(word, chars) -> bool:
     neighbors = find_neighboring_characters_in_word(word, chars)
-    if neighbors is not None:
-        return not consonant(neighbors["preceding_character"])
+    return neighbors and not consonant(neighbors["preceding_character"])
 
-def before_consonant(word, chars) -> bool:
+def before_vowel(word, chars) -> bool:
     neighbors = find_neighboring_characters_in_word(word, chars)
-    if neighbors is not None:
-        return not consonant(neighbors["trailing_character"])
+    return neighbors and not consonant(neighbors["trailing_character"])
 
-def not_a_triple_consonant_cluster(word, chars) -> bool:
+def is_a_two_consonant_cluster(word, chars) -> bool:
     # Triple consonants are either loan words (abstraktio for example from
     # English) or have a syllabic border (punt-ti).
     # They are calculated with a separate script.
-    chars_length = len(chars)
-    return chars_length == 2 \
-        and not after_consonant(word, chars) \
-        and not before_consonant(word, chars)
+
+    return  len(chars) == 2 \
+        and chars in word \
+        and after_vowel(word, chars) \
+        and before_vowel(word, chars)
 
 def not_a_name_or_triple_consonant_cluster(word, chars):
-    analysis.not_a_name(word) and not_a_triple_consonant_cluster(word, chars)
+    analysis.not_a_name(word) and is_a_two_consonant_cluster(word, chars)
 
 def main():
     consonant_permutations = tools.all_character_pairs(consonants, length = 2)
--- word-analyser/word_analyser/test_double_consonant_frequencies.py
+++ word-analyser/word_analyser/test_double_consonant_frequencies.py
@@ -1,18 +1,32 @@
 import unittest
 from ensure import ensure
-from .double_consonant_frequencies import not_a_triple_consonant_cluster
+from .double_consonant_frequencies import *
 
 class TestDoubleConsonantFrequencies(unittest.TestCase):
-    def test_not_a_triple_consonant_cluster(self):
-       ensure(not_a_triple_consonant_cluster(word = "a", chars = "b")).is_(False)
-       ensure(not_a_triple_consonant_cluster("a", "bb")).is_(True)
-       ensure(not_a_triple_consonant_cluster("abstraktio", "bb")).is_(True)
+    def test_after_vowel(self):
+        ensure(after_vowel("abstraktio", "st")).is_(False)
 
-       # in the middle of a cluster
-       ensure(not_a_triple_consonant_cluster("abstraktio", "st")).is_(False)
+    def test_is_a_double_consonant_cluster(self):
+       ensure(is_a_two_consonant_cluster(word = "a", chars = "b")).is_(False)
+       ensure(is_a_two_consonant_cluster("a", "bb")).is_(False)
 
-       # at the start
-       ensure(not_a_triple_consonant_cluster("abstraktio", "bs")).is_(False)
+       # # not included in word
+       ensure(is_a_two_consonant_cluster("abstraktio", "bb")).is_(False)
 
-       # at the end
-       ensure(not_a_triple_consonant_cluster("abstraktio", "tr")).is_(False)
+       # # in the middle of a cluster
+       ensure(is_a_two_consonant_cluster("abstraktio", "st")).is_(False)
+
+       # # at the start
+       ensure(is_a_two_consonant_cluster("abstraktio", "bs")).is_(False)
+
+       # # at the end
+       ensure(is_a_two_consonant_cluster("abstraktio", "tr")).is_(False)
+       ensure(is_a_two_consonant_cluster("arktinen", "kt")).is_(False)
+
+       # happy path
+       ensure(is_a_two_consonant_cluster("abstraktio", "kt")).is_(True)
+       ensure(is_a_two_consonant_cluster("akti", "kt")).is_(True)
+
+    def test_find_neighboring_characters_in_word(self):
+        expected = {'preceding_character': 'a', 'trailing_character': 'i'}
+        ensure(find_neighboring_characters_in_word("abstraktio", "kt")).equals(expected)
