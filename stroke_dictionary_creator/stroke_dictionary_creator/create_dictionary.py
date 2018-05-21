6441c4a5dcfbb7c20a72be2df76349737fd168db
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-21 16:41:40 +0300

    Report progress in dictionary creation script

--- stroke_dictionary_creator/stroke_dictionary_creator/create_dictionary.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/create_dictionary.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+import os
 from ruamel.yaml import YAML
 import glob
 from toolz import functoolz
@@ -7,6 +8,7 @@ from functools import reduce
 import json
 
 def write_as_json_stroke_dictionary(destination, flat_stroke_dictionary):
+    print("Writing output to: %s" % destination)
     with open(destination, "w") as f:
         json.dump(flat_stroke_dictionary,
                   f,
@@ -16,6 +18,9 @@ def write_as_json_stroke_dictionary(destination, flat_stroke_dictionary):
 
 def reverse_keys_and_values(dictionaries):
     return {v: k for k, v in dictionaries.items()}
+
+def validate(dictionary):
+    return dictionary
 
 def combine(dictionaries):
     # in case of conflicts, keeps the values specified earlier in the order of
@@ -27,7 +32,10 @@ def combine(dictionaries):
 
 def load_file_as_yaml(filepath):
     with open(filepath) as f:
-        return YAML().load(f)
+        d = YAML().load(f)
+        name = os.path.basename(filepath)
+        print("Loaded %s with %s strokes." % (name, len(d)))
+        return d
 
 def load_dictionaries_from_path(path):
     yaml = YAML()
@@ -37,17 +45,21 @@ def load_dictionaries_from_path(path):
 
 def main():
     input_path = "../input_dictionaries/"
+    print()
     print("Reading input yaml dictionaries from: %s" % input_path)
 
     flat_dictionary = functoolz.thread_first(
         load_dictionaries_from_path(input_path),
+        validate,
         combine,
         reverse_keys_and_values)
 
-    output_path = "../output/plover_finnish_output.json"
+    output_path = "../../plugin/plover_finnish/plover_finnish/dictionaries/plover_finnish.json"
+    print("")
     print("Input dictionaries read.")
-    print("Writing output to: %s" % output_path)
     write_as_json_stroke_dictionary(output_path, flat_dictionary)
+    print("")
+    print("%s strokes total." % len(flat_dictionary))
     return 0 # no error
 
 if __name__ == '__main__':
