2bff8eb7b13c5d6215dffb3d4b3cfafa9a7a2c0c
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-25 09:22:28 +0300

    Add some word parsers

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/generators/short_words.py
@@ -0,0 +1,16 @@
+from parsy import *
+
+from stroke_dictionary_creator.generators.generators import *
+
+no_middle_key = success("-")
+
+# all words of length 2
+two_vowel_word       = middle_vowel + end_vowel
+vowel_consonant_word = middle_vowel + final_consonant
+consonant_vowel_word = initial_consonant + no_middle_key + end_vowel
+
+vowel_end_diphtong_word    = middle_vowel + end_diphtong
+consonant_vowel_vowel_word = initial_consonant + middle_vowel + end_vowel
+consonant_vowel_c_w_word   = initial_consonant + middle_vowel + final_consonant + end_vowel
+# TODO test
+vowel_consonant_v_v_word   = middle_vowel + final_consonant + end_diphtong
---
+++ stroke_dictionary_creator/stroke_dictionary_creator/generators/test_short_words.py
@@ -0,0 +1,22 @@
+import unittest
+from ensure import ensure
+from stroke_dictionary_creator.generators.short_words import *
+
+class TestShortWords(unittest.TestCase):
+    def test_two_vowel_word(self):
+        ensure(two_vowel_word.parse("ai")).equals("Ai")
+
+    def test_vowel_consonant_word(self):
+        ensure(vowel_consonant_word.parse("on")).equals("ON")
+
+    def test_consonant_vowel_word(self):
+        ensure(consonant_vowel_word.parse("no")).equals("TPH-o")
+
+    def test_consonant_vowel_vowel_word(self):
+        ensure(consonant_vowel_vowel_word.parse("joo")).equals("SKVROo")
+
+    def test_vowel_end_diphtong_word(self):
+        ensure(vowel_end_diphtong_word.parse("oja")).equals("Oia")
+
+    def test_consonant_vowel_c_w_word(self):
+        ensure(consonant_vowel_c_w_word.parse("tina")).equals("TINa")
