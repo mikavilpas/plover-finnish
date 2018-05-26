ef946273e0acfb4ab5707ecb65671e645bf44cca
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-26 09:39:46 +0300

    Add parsers for different word classes

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection_service.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection_service.py
@@ -7,12 +7,36 @@ not_available = lambda inflections: inflections == {}
 
 word_char = letter | whitespace | digit | char_from(".,-_")
 
+no_inflection = None
+
+# the classes require a specific format in Finnish that must be honored
+noun      = string("noun").result("subst")
+adjective = string("adjective").result("subst") # some forms are handled as nouns in the inflector
+verb      = string("verb").result("verbi")
+
+pronoun_fst   = string("pnoun_firstname").result("subst")
+pronoun_lst   = string("pnoun_lastname").result("subst")
+pronoun_misc  = string("pnoun_misc").result("subst")
+pronoun_place = string("pnoun_place").result("subst")
+
+pronoun = pronoun_fst | pronoun_lst | pronoun_misc
+
+class_with_no_inflection = string_from("abbreviation",
+                                       "adverb",
+                                       "conjunction",
+                                       "interjection").result(no_inflection)
+
+word_class = noun | adjective | verb | pronoun | class_with_no_inflection
+
 @generate
 def word_and_class():
     word = yield word_char.at_least(1).concat()
     yield string(";")
-    classes = yield word_char.at_least(1).concat()
-    return [word, classes]
+    klass = yield word_class
+    yield string("-")
+    reference_word = yield word_char.at_least(1).concat()
+
+    return [word, klass + "-" + reference_word]
 
 def inflected_forms(word_and_infclass):
     (word, infclass) = word_and_class.parse(word_and_infclass)
--- stroke_dictionary_creator/stroke_dictionary_creator/test_inflection_service.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/test_inflection_service.py
@@ -6,10 +6,10 @@ class TestInflectionService(unittest.TestCase):
 
     def test_inflected_forms(self):
         # must be able to distinguish nouns ans verbos based on the class of the word
-        forms = inflected_forms("koe", infclass = "subst-hame-av6")
+        forms = inflected_forms("koe;noun-hame-av6")
         ensure(forms["genetiivi"]).equals("kokeen")
 
-        forms = inflected_forms("koettaa", infclass = "verbi-aavistaa-av1")
+        forms = inflected_forms("koettaa;verb-aavistaa-av1")
         ensure(forms["imperfekti_pass"]).equals("koetettiin")
 
         # fails for irregular words, which have to be added manually. There are
@@ -19,11 +19,11 @@ class TestInflectionService(unittest.TestCase):
 
     def test_word_and_class_parser(self):
         ensure(word_and_class.parse("koe;noun-hame-av6"))\
-            .equals(["koe", "noun-hame-av6"])
+            .equals(["koe", "subst-hame-av6"])
 
         # can parse umlauts
         ensure(word_and_class.parse("käheä;adjective-korkea"))\
-            .equals(["käheä", "adjective-korkea"])
+            .equals(["käheä", "subst-korkea"])
 
         # can parse underscores
         ensure(word_and_class.parse("ARA;pnoun_misc-kala"))\
