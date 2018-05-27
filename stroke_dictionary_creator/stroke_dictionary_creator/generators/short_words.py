c807ad7109b98cbe898ee0277c76376c8eecc066
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-27 18:01:14 +0300

    Allow all word parsers to end in a end vowel sound of some kind

--- stroke_dictionary_creator/stroke_dictionary_creator/generators/short_words.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/generators/short_words.py
@@ -11,14 +11,14 @@ word_cv = initial_consonant + no_middle_key + end_vowel
 
 word_v_end_diphtong = middle_vowel + end_diphtong
 
-word_vcv = middle_vowel + final_consonant + end_vowel
-word_cvv = initial_consonant + middle_vowel + end_vowel
+word_vcv = middle_vowel + final_consonant + end_vocal_sound
+word_cvv = initial_consonant + middle_vowel + end_vocal_sound
 word_cvc = initial_consonant + middle_vowel + final_consonant
 
-word_cvcv = initial_consonant + middle_vowel + final_consonant + end_vowel
-word_vccv = middle_vowel + final_two_consonants + end_vowel
-word_vcvv = middle_vowel + final_consonant + end_diphtong
-word_vvcv = (long_vowel | middle_diphtong) + final_consonant + end_vowel
+word_cvcv = initial_consonant + middle_vowel + final_consonant + end_vocal_sound
+word_vccv = middle_vowel + final_two_consonants + end_vocal_sound
+word_vcvv = middle_vowel + final_consonant + end_vocal_sound
+word_vvcv = (long_vowel | middle_diphtong) + final_consonant + end_vocal_sound
 
 short_word = word_vcvv | word_vvcv | word_cvcv | word_vccv | word_cvc | \
              word_cvv | word_vcv | word_v_end_diphtong | \
