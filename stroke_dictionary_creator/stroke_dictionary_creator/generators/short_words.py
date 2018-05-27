96ad04372bce57ed70880cfeacae724ece64cd75
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-27 18:13:42 +0300

    Recognize the word äijä

--- stroke_dictionary_creator/stroke_dictionary_creator/generators/short_words.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/generators/short_words.py
@@ -5,11 +5,11 @@ from .generators import *
 no_middle_key = success("-")
 
 # all words of length 2
-word_vv = middle_vowel + end_vowel
+word_vv = middle_vowel + end_vocal_sound
 word_vc = middle_vowel + final_consonant
-word_cv = initial_consonant + no_middle_key + end_vowel
+word_cv = initial_consonant + no_middle_key + end_vocal_sound
 
-word_v_end_diphtong = middle_vowel + end_diphtong
+word_v_end_diphtong = middle_vowel + end_vocal_sound
 
 word_vcv = middle_vowel + final_consonant + end_vocal_sound
 word_cvv = initial_consonant + middle_vowel + end_vocal_sound
@@ -20,6 +20,7 @@ word_vccv = middle_vowel + final_two_consonants + end_vocal_sound
 word_vcvv = middle_vowel + final_consonant + end_vocal_sound
 word_vvcv = (long_vowel | middle_diphtong) + final_consonant + end_vocal_sound
 
+# short_word_2 = word_vv
 short_word = word_vcvv | word_vvcv | word_cvcv | word_vccv | word_cvc | \
              word_cvv | word_vcv | word_v_end_diphtong | \
              word_cv | word_vc | word_vv
