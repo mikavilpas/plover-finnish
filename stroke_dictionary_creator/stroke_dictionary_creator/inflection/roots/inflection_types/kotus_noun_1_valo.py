f5c8605e2d5f8a7601503baaff0f6f699978c232
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-29 16:13:15 +0300

    Move more files

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_1_valo.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_1_valo.py
@@ -1,17 +1,4 @@
-from parsy import *
-from ..tokens import *
-from ..noun_inflection_info import InflectionInfo
-from ..gradation import identity
-from ..parse_utils import reverse_parse
-
-sys.path.append("../../../../plugin/plover_finnish/")
-from plover_finnish.vowel_group_service import change_to_same_vowel_group
-
-@generate
-def root_and_end_vowel():
-    end_vowel = yield vowel
-    rest = yield character.at_least(1).concat()
-    return [rest, end_vowel]
+from .base import *
 
 def kotus_noun_1_valo(word, gradation_fn = identity):
     # suffix
