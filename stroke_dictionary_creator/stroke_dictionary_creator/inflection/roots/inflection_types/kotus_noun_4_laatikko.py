f5854164150a4b667de68b1f215a987cef316c65
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-30 10:12:09 +0300

    Add class 4 (laatikko)

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_4_laatikko.py
@@ -0,0 +1,21 @@
+from .base import *
+from .kotus_noun_1_valo import kotus_noun_1_valo
+
+def kotus_noun_4_laatikko(word, gradation_fn = identity):
+    # suffix
+    def s(text): return change_to_same_vowel_group(word, text)
+
+    word_alt = gradation_fn(word)
+    (root, end_vowel) = reverse_parse(word, root_and_end_vowel)
+    inflections = kotus_noun_1_valo(word, gradation_fn)
+    return inflections._replace(genitives_plural = [word_alt + "iden",
+                                                    word_alt + "itten",
+                                                    word + "jen"],
+                                partitive = word + s("a"),
+                                partitives_plural = [word + s("ja"),
+                                                     word_alt + s("ita")],
+                                illative = word + end_vowel + "n",
+                                illatives_plural = [word + "ihin",
+                                                    word_alt + "ihin"],
+                                essive_plural = word_alt + s("ina"),
+                                accusative_plural = word_alt + "t")
---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_4_laatikko.py
@@ -0,0 +1,60 @@
+import unittest
+from ensure import ensure
+from .kotus_noun_4_laatikko import *
+from ..gradation import *
+
+class TestInflectionType4(unittest.TestCase):
+    # all the cases seem to belong to the gradation class A
+
+    def test_kotus_noun_4_laatikko_full_cases(self):
+        data = kotus_noun_4_laatikko("laatikko", gradate_kotus_a_takki_takin_hake_hakkeen)
+
+        ensure(data.nominative).equals("laatikko")
+        ensure(data.nominative_plural).equals("laatikot")
+
+        ensure(data.genitive).equals("laatikon")
+        ensure(data.genitives_plural).equals(["laatikoiden",
+                                              "laatikoitten",
+                                              "laatikkojen"])
+
+        ensure(data.partitive).equals("laatikkoa")
+        ensure(data.partitives_plural).equals(["laatikkoja", "laatikoita"])
+
+        ensure(data.accusatives).equals(["laatikko", "laatikon"])
+        ensure(data.accusative_plural).equals("laatikot")
+
+        ensure(data.inessive).equals("laatikossa")
+        ensure(data.inessive_plural).equals("laatikoissa")
+
+        ensure(data.elative).equals("laatikosta")
+        ensure(data.elative_plural).equals("laatikoista")
+
+        ensure(data.illative).equals("laatikkoon")
+        ensure(data.illatives_plural).equals(["laatikkoihin", "laatikoihin"])
+
+        ensure(data.adessive).equals("laatikolla")
+        ensure(data.adessive_plural).equals("laatikoilla")
+
+        ensure(data.ablative).equals("laatikolta")
+        ensure(data.ablative_plural).equals("laatikoilta")
+
+        ensure(data.allative).equals("laatikolle")
+        ensure(data.allative_plural).equals("laatikoille")
+
+        ensure(data.essive).equals("laatikkona")
+        ensure(data.essive_plural).equals("laatikoina")
+
+        ensure(data.translative).equals("laatikoksi")
+        ensure(data.translative_plural).equals("laatikoiksi")
+
+        ensure(data.abessive).equals("laatikotta")
+        ensure(data.abessive_plural).equals("laatikoitta")
+
+        ensure(data.instructive_plural).equals("laatikoin")
+
+        ensure(data.comitative_plural).equals("laatikkoine")
+
+    def test_kotus_noun_4_laatikko_umlauts(self):
+        data = kotus_noun_4_laatikko("älykkö", gradate_kotus_a_takki_takin_hake_hakkeen)
+        ensure(data.genitives_plural).equals(["älyköiden", "älyköitten", "älykköjen"])
+        print(data)
