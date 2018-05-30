b31ce1af8e8da22277f1388877de4d4d6be9b664
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-30 16:40:33 +0300

    Simplify class 1 test

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_1_valo.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_1_valo.py
@@ -2,53 +2,42 @@ import unittest
 from ensure import ensure
 from .kotus_noun_1_valo import *
 from ..gradation import *
+from .test_utils import ensure_inflections_equal
 
 class TestInflectionType1(unittest.TestCase):
     def test_kotus_noun_1_valo_full_cases(self):
         data = kotus_noun_1_valo("aamu")
 
-        ensure(data.nominative).equals("aamu")
-        ensure(data.nominative_plural).equals("aamut")
-
-        ensure(data.genitive).equals("aamun")
-        ensure(data.genitives_plural).equals(["aamujen"])
-
-        ensure(data.partitive).equals("aamua")
-        ensure(data.partitives_plural).equals(["aamuja"])
-
-        ensure(data.accusatives).equals(["aamu", "aamun"])
-        ensure(data.accusative_plural).equals("aamuja")
-
-        ensure(data.inessive).equals("aamussa")
-        ensure(data.inessive_plural).equals("aamuissa")
-
-        ensure(data.elative).equals("aamusta")
-        ensure(data.elative_plural).equals("aamuista")
-
-        ensure(data.illative).equals("aamuun")
-        ensure(data.illatives_plural).equals(["aamuihin"])
-
-        ensure(data.adessive).equals("aamulla")
-        ensure(data.adessive_plural).equals("aamuilla")
-
-        ensure(data.ablative).equals("aamulta")
-        ensure(data.ablative_plural).equals("aamuilta")
-
-        ensure(data.allative).equals("aamulle")
-        ensure(data.allative_plural).equals("aamuille")
-
-        ensure(data.essive).equals("aamuna")
-        ensure(data.essive_plural).equals("aamuina")
-
-        ensure(data.translative).equals("aamuksi")
-        ensure(data.translative_plural).equals("aamuiksi")
-
-        ensure(data.abessive).equals("aamutta")
-        ensure(data.abessive_plural).equals("aamuitta")
-
-        ensure(data.instructive_plural).equals("aamuin")
-
-        ensure(data.comitative_plural).equals("aamuine")
+        expected = InflectionInfo(nominative='aamu',
+                                  nominative_plural='aamut',
+                                  genitive='aamun',
+                                  genitives_plural=['aamujen'],
+                                  partitive='aamua',
+                                  partitives_plural=['aamuja'],
+                                  accusatives=['aamu',
+                                               'aamun'],
+                                  accusative_plural='aamuja',
+                                  inessive='aamussa',
+                                  inessive_plural='aamuissa',
+                                  elative='aamusta',
+                                  elative_plural='aamuista',
+                                  illative='aamuun',
+                                  illatives_plural=['aamuihin'],
+                                  adessive='aamulla',
+                                  adessive_plural='aamuilla',
+                                  ablative='aamulta',
+                                  ablative_plural='aamuilta',
+                                  allative='aamulle',
+                                  allative_plural='aamuille',
+                                  essive='aamuna',
+                                  essive_plural='aamuina',
+                                  translative='aamuksi',
+                                  translative_plural='aamuiksi',
+                                  abessive='aamutta',
+                                  abessive_plural='aamuitta',
+                                  instructive_plural='aamuin',
+                                  comitative_plural='aamuine')
+        ensure_inflections_equal(expected, data)
 
     def test_kotus_noun_1_valo_umlauts(self):
         data = kotus_noun_1_valo("emännistö")
