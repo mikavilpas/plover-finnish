c3137b5326046d870c6c6baa2d475bbc39b4423b
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-30 17:38:04 +0300

    Clean up tests for class 4

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_4_laatikko.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_4_laatikko.py
@@ -1,58 +1,49 @@
 import unittest
 from ensure import ensure
-from .kotus_noun_4_laatikko import *
-from ..gradation import *
+from .kotus_noun_4_laatikko import kotus_noun_4_laatikko
+from ..noun_inflection_info import InflectionInfo
+from .test_utils import ensure_inflections_equal
+from ..gradation import gradate_kotus_a_takki_takin_hake_hakkeen
 
 class TestInflectionType4(unittest.TestCase):
     # all the cases seem to belong to the gradation class A
 
     def test_kotus_noun_4_laatikko_full_cases(self):
         data = kotus_noun_4_laatikko("laatikko", gradate_kotus_a_takki_takin_hake_hakkeen)
-
-        ensure(data.nominative).equals("laatikko")
-        ensure(data.nominative_plural).equals("laatikot")
-
-        ensure(data.genitive).equals("laatikon")
-        ensure(data.genitives_plural).equals(["laatikoiden",
-                                              "laatikoitten",
-                                              "laatikkojen"])
-
-        ensure(data.partitive).equals("laatikkoa")
-        ensure(data.partitives_plural).equals(["laatikkoja", "laatikoita"])
-
-        ensure(data.accusatives).equals(["laatikko", "laatikon"])
-        ensure(data.accusative_plural).equals("laatikot")
-
-        ensure(data.inessive).equals("laatikossa")
-        ensure(data.inessive_plural).equals("laatikoissa")
-
-        ensure(data.elative).equals("laatikosta")
-        ensure(data.elative_plural).equals("laatikoista")
-
-        ensure(data.illative).equals("laatikkoon")
-        ensure(data.illatives_plural).equals(["laatikkoihin", "laatikoihin"])
-
-        ensure(data.adessive).equals("laatikolla")
-        ensure(data.adessive_plural).equals("laatikoilla")
-
-        ensure(data.ablative).equals("laatikolta")
-        ensure(data.ablative_plural).equals("laatikoilta")
-
-        ensure(data.allative).equals("laatikolle")
-        ensure(data.allative_plural).equals("laatikoille")
-
-        ensure(data.essive).equals("laatikkona")
-        ensure(data.essive_plural).equals("laatikoina")
-
-        ensure(data.translative).equals("laatikoksi")
-        ensure(data.translative_plural).equals("laatikoiksi")
-
-        ensure(data.abessive).equals("laatikotta")
-        ensure(data.abessive_plural).equals("laatikoitta")
-
-        ensure(data.instructive_plural).equals("laatikoin")
-
-        ensure(data.comitative_plural).equals("laatikkoine")
+        expected = InflectionInfo(nominative='laatikko',
+                                  nominative_plural='laatikot',
+                                  genitive='laatikon',
+                                  genitives_plural=['laatikoiden',
+                                                    'laatikoitten',
+                                                    'laatikkojen'],
+                                  partitive='laatikkoa',
+                                  partitives_plural=['laatikkoja',
+                                                     'laatikoita'],
+                                  accusatives=['laatikko',
+                                               'laatikon'],
+                                  accusative_plural='laatikot',
+                                  inessive='laatikossa',
+                                  inessive_plural='laatikoissa',
+                                  elative='laatikosta',
+                                  elative_plural='laatikoista',
+                                  illative='laatikkoon',
+                                  illatives_plural=['laatikkoihin',
+                                                    'laatikoihin'],
+                                  adessive='laatikolla',
+                                  adessive_plural='laatikoilla',
+                                  ablative='laatikolta',
+                                  ablative_plural='laatikoilta',
+                                  allative='laatikolle',
+                                  allative_plural='laatikoille',
+                                  essive='laatikkona',
+                                  essive_plural='laatikoina',
+                                  translative='laatikoksi',
+                                  translative_plural='laatikoiksi',
+                                  abessive='laatikotta',
+                                  abessive_plural='laatikoitta',
+                                  instructive_plural='laatikoin',
+                                  comitative_plural='laatikkoine')
+        ensure_inflections_equal(expected, data)
 
     def test_kotus_noun_4_laatikko_umlauts(self):
         data = kotus_noun_4_laatikko("älykkö", gradate_kotus_a_takki_takin_hake_hakkeen)
