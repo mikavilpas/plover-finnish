465984572f9eddd1300ff403f233cf47185af6a6
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-30 09:25:14 +0300

    Simplify class 2 implementation

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_2_palvelu.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_2_palvelu.py
@@ -1,43 +1,14 @@
 from .base import *
+from .kotus_noun_1_valo import kotus_noun_1_valo
 
 def kotus_noun_2_palvelu(word, gradation_fn = identity):
     # suffix
     def s(text): return change_to_same_vowel_group(word, text)
 
-    (root, end_vowel) = reverse_parse(word, root_and_end_vowel)
-    word_alt = gradation_fn(word)
-
-    v = end_vowel
-    return InflectionInfo(nominative         = word,
-                          nominative_plural  = word_alt + "t",
-                          genitive           = word_alt + "n",
-                          genitives_plural   = [word + "jen",
-                                                word + "iden",
-                                                word + "itten"],
-                          partitive          = word + s("a"),
-                          partitives_plural  = [word + s("ita"),
-                                                word + s("ja")],
-                          accusatives        = [word,
-                                                word_alt + "n"],
-                          accusative_plural  = word + "t",
-                          inessive           = word_alt + s("ssa"),
-                          inessive_plural    = word_alt + s("issa"),
-                          elative            = word_alt + s("sta"),
-                          elative_plural     = word_alt + s("ista"),
-                          illative           = word + v + "n",
-                          illative_plural    = word + "ihin",
-                          adessive           = word_alt + s("lla"),
-                          adessive_plural    = word_alt + s("illa"),
-                          ablative           = word_alt + s("lta"),
-                          ablative_plural    = word_alt + s("ilta"),
-                          allative           = word_alt + "lle",
-                          allative_plural    = word_alt + "ille",
-                          essive             = word + s("na"),
-                          essive_plural      = word + s("ina"),
-                          translative        = word_alt + "ksi",
-                          translative_plural = word_alt + "iksi",
-                          abessive           = word_alt + s("tta"),
-                          abessive_plural    = word_alt + s("itta"),
-                          instructive_plural = word_alt + "in",
-                          comitative_plural  = word + "ine",
-    )
+    inflections = kotus_noun_1_valo(word, gradation_fn)
+    return inflections._replace(genitives_plural = [word + "jen",
+                                                    word + "iden",
+                                                    word + "itten"],
+                                partitives_plural = [word + s("ita"),
+                                                     word + s("ja")],
+                                accusative_plural = word + "t")
