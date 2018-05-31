f57036abb16d55ee45c180f428b8c6be2114cf36
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-31 19:31:25 +0300

    Fix class 2

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_2_palvelu.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_2_palvelu.py
@@ -7,7 +7,6 @@ from .test_utils import ensure_inflections_equal
 class TestInflectionType2(unittest.TestCase):
     def test_kotus_noun_2_palvelu_full_cases(self):
         data = kotus_noun_2_palvelu("palvelu")
-        print(data)
         expected = InflectionInfo(nominative='palvelu',
                                   nominative_plural='palvelut',
                                   genitive='palvelun',
@@ -21,30 +20,30 @@ class TestInflectionType2(unittest.TestCase):
                                                'palvelun'],
                                   accusative_plural='palvelut',
                                   inessive='palvelussa',
-                                  inessive_plural='palveluissa',
+                                  inessives_plural=['palveluissa'],
                                   elative='palvelusta',
-                                  elative_plural='palveluista',
+                                  elatives_plural=['palveluista'],
                                   illative='palveluun',
                                   illatives_plural=['palveluihin'],
                                   adessive='palvelulla',
-                                  adessive_plural='palveluilla',
+                                  adessives_plural=['palveluilla'],
                                   ablative='palvelulta',
-                                  ablative_plural='palveluilta',
+                                  ablatives_plural=['palveluilta'],
                                   allative='palvelulle',
-                                  allative_plural='palveluille',
+                                  allatives_plural=['palveluille'],
                                   essive='palveluna',
-                                  essive_plural='palveluina',
+                                  essives_plural=['palveluina'],
                                   translative='palveluksi',
-                                  translative_plural='palveluiksi',
+                                  translatives_plural=['palveluiksi'],
                                   abessive='palvelutta',
-                                  abessive_plural='palveluitta',
-                                  instructive_plural='palveluin',
-                                  comitative_plural='palveluine')
+                                  abessives_plural=['palveluitta'],
+                                  instructives_plural=['palveluin'],
+                                  comitatives_plural=['palveluine'])
         ensure_inflections_equal(expected, data)
 
     def test_kotus_noun_2_palvelu_umlauts(self):
         data = kotus_noun_2_palvelu("epäily")
-        ensure(data.comitative_plural).equals("epäilyine")
+        ensure(data.comitatives_plural).equals(["epäilyine"])
 
     # The kotus wordlist does not contain any words that belong to class 2 and
     # have a gradation class. Have to skip testing that.
