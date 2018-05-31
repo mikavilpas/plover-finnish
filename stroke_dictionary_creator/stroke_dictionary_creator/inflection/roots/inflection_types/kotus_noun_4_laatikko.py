d17345dc368d3c55866997a049adf9e370751aab
Mika Vilpas <mika.vilpas@gmail.com>
2018-05-31 20:22:07 +0300

    Fix class 4

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_4_laatikko.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_4_laatikko.py
@@ -8,14 +8,14 @@ def kotus_noun_4_laatikko(word, gradation_fn = identity):
     word_alt = gradation_fn(word)
     (root, end_vowel) = reverse_parse(word, root_and_end_vowel)
     inflections = kotus_noun_1_valo(word, gradation_fn)
-    return inflections._replace(genitives_plural = [word_alt + "iden",
-                                                    word_alt + "itten",
-                                                    word + "jen"],
-                                partitive = word + s("a"),
+    return inflections._replace(genitives_plural  = [word_alt + "iden",
+                                                     word_alt + "itten",
+                                                     word + "jen"],
+                                partitive         = word + s("a"),
                                 partitives_plural = [word + s("ja"),
                                                      word_alt + s("ita")],
-                                illative = word + end_vowel + "n",
-                                illatives_plural = [word + "ihin",
-                                                    word_alt + "ihin"],
-                                essive_plural = word_alt + s("ina"),
+                                illative          = word + end_vowel + "n",
+                                illatives_plural  = [word + "ihin",
+                                                     word_alt + "ihin"],
+                                essives_plural    = [word_alt + s("ina")],
                                 accusative_plural = word_alt + "t")
