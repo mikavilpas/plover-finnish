904274cd7496a679275c41116d18596fb8dfe2de
Mika Vilpas <mika.vilpas@gmail.com>
2018-06-04 22:49:39 +0300

    Add class 16 (vanhempi)

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_16_vanhempi.py
@@ -0,0 +1,44 @@
+from .base import *
+from .kotus_noun_13_katiska import kotus_noun_13_katiska
+
+def kotus_noun_16_vanhempi(word, gradation_fn = identity):
+    # suffix
+    def s(text): return change_to_same_vowel_group_prefer_umlauts(word, text)
+
+    word_alt = gradation_fn(word)
+
+    # all words in this class end in "a" or "ä"
+    (root, end_vowel) = reverse_parse(word, root_and_end_vowel)
+    (root_alt, _) = reverse_parse(word_alt, root_and_end_vowel)
+    v = end_vowel
+
+    reference = kotus_noun_13_katiska(word, gradation_fn)
+    return reference._replace(nominative_plural   = root_alt + s("at"),
+                              genitive            = root_alt + s("an"),
+                              genitives_plural    = [root + s("ien"),
+                                                     root + s("ain")],
+                              partitives          = [root + s("aa")],
+                              partitives_plural   = [root + s("ia")],
+                              accusatives         = [word,
+                                                     root_alt + s("an")],
+                              accusative_plural   = root_alt + s("at"),
+                              inessive            = root_alt + s("assa"),
+                              inessives_plural    = [root_alt + s("issa")],
+                              elative             = root_alt + s("asta"),
+                              elatives_plural     = [root_alt + s("ista")],
+                              illative            = root + s("aan"),
+                              illatives_plural    = [root + s("iin")],
+                              adessive            = root_alt + s("alla"),
+                              adessives_plural    = [root_alt + s("illa")],
+                              ablative            = root_alt + s("alta"),
+                              ablatives_plural    = [root_alt + s("ilta")],
+                              allative            = root_alt + s("alle"),
+                              allatives_plural    = [root_alt + s("ille")],
+                              essive              = root + s("ana"),
+                              essives_plural      = [root + s("ina")],
+                              translative         = root_alt + s("aksi"),
+                              translatives_plural = [root_alt + s("iksi")],
+                              abessive            = root_alt + s("atta"),
+                              abessives_plural    = [root_alt + s("itta")],
+                              instructives_plural = [root_alt + s("in")],
+                              comitatives_plural  = [root + s("ine")],)
---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_16_vanhempi.py
@@ -0,0 +1,84 @@
+import unittest
+from ensure import ensure
+from ..noun_inflection_info import InflectionInfo
+from .test_utils import ensure_inflections_equal
+from ..gradation import gradate_kotus_h_kumpi_kumman_lumme_lumpeen
+from .kotus_noun_16_vanhempi import kotus_noun_16_vanhempi
+
+class TestInflectionType16(unittest.TestCase):
+    def test_basic_example(self):
+        data = kotus_noun_16_vanhempi("vanhempi",
+                                      gradate_kotus_h_kumpi_kumman_lumme_lumpeen)
+
+        expected = InflectionInfo(nominative="vanhempi",
+                                  nominative_plural   = "vanhemmat",
+                                  genitive            = "vanhemman",
+                                  genitives_plural    = ["vanhempien",
+                                                         "vanhempain"],
+                                  partitives          = ["vanhempaa"],
+                                  partitives_plural   = ["vanhempia"],
+                                  accusatives         = ["vanhempi", "vanhemman"],
+                                  accusative_plural   = "vanhemmat",
+                                  inessive            = "vanhemmassa",
+                                  inessives_plural    = ["vanhemmissa"],
+                                  elative             = "vanhemmasta",
+                                  elatives_plural     = ["vanhemmista"],
+                                  illative            = "vanhempaan",
+                                  illatives_plural    = ["vanhempiin"],
+                                  adessive            = "vanhemmalla",
+                                  adessives_plural    = ["vanhemmilla"],
+                                  ablative            = "vanhemmalta",
+                                  ablatives_plural    = ["vanhemmilta"],
+                                  allative            = "vanhemmalle",
+                                  allatives_plural    = ["vanhemmille"],
+                                  essive              = "vanhempana",
+                                  essives_plural      = ["vanhempina"],
+                                  translative         = "vanhemmaksi",
+                                  translatives_plural = ["vanhemmiksi"],
+                                  abessive            = "vanhemmatta",
+                                  abessives_plural    = ["vanhemmitta"],
+                                  instructives_plural = ["vanhemmin"],
+                                  comitatives_plural  = ["vanhempine"])
+
+        ensure_inflections_equal(expected, data)
+
+    def test_with_umlauts(self):
+        data = kotus_noun_16_vanhempi("vähempi",
+                                      gradate_kotus_h_kumpi_kumman_lumme_lumpeen)
+
+        expected = InflectionInfo(nominative='vähempi',
+                                  nominative_plural='vähemmät',
+                                  genitive='vähemmän',
+                                  genitives_plural=['vähempien',
+                                                    'vähempäin'],
+                                  partitives=['vähempää'],
+                                  partitives_plural=['vähempiä'],
+                                  accusatives=['vähempi',
+                                               'vähemmän'],
+                                  accusative_plural='vähemmät',
+                                  inessive='vähemmässä',
+                                  inessives_plural=['vähemmissä'],
+                                  elative='vähemmästä',
+                                  elatives_plural=['vähemmistä'],
+                                  illative='vähempään',
+                                  illatives_plural=['vähempiin'],
+                                  adessive='vähemmällä',
+                                  adessives_plural=['vähemmillä'],
+                                  ablative='vähemmältä',
+                                  ablatives_plural=['vähemmiltä'],
+                                  allative='vähemmälle',
+                                  allatives_plural=['vähemmille'],
+                                  essive='vähempänä',
+                                  essives_plural=['vähempinä'],
+                                  translative='vähemmäksi',
+                                  translatives_plural=['vähemmiksi'],
+                                  abessive='vähemmättä',
+                                  abessives_plural=['vähemmittä'],
+                                  instructives_plural=['vähemmin'],
+                                  comitatives_plural=['vähempine'])
+
+        ensure_inflections_equal(expected, data)
+
+
+    # all words with gradation in this class use the gradation class h
+    # no words end in a consonant.
