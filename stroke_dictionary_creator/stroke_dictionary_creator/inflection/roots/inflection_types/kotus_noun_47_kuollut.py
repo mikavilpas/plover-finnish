4b0b333321d1357630655c6ae06c663f3aa9e167
Mika Vilpas <mika.vilpas@gmail.com>
2018-06-10 15:13:17 +0300

    Add class 47 (kuollut)

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_47_kuollut.py
@@ -0,0 +1,53 @@
+from .base import *
+
+from parsy import *
+from ..tokens import *
+
+@generate
+def word_vowel_consonant_ending():
+    # words in this class end in "-ut"
+    c = yield consonant
+    v = yield vowel
+    root = yield character.at_least(1).concat()
+    return [root, v + c]
+
+def kotus_noun_47_kuollut(word, gradation_fn = identity):
+    # suffix
+    def s(text): return change_to_same_vowel_group_prefer_umlauts(word, text)
+
+    word_alt = gradation_fn(word)
+
+    (root, on_ending) = reverse_parse(word, word_vowel_consonant_ending)
+    v = ""
+
+    return InflectionInfo(nominative          = word,
+                          nominative_plural   = root + s("eet"),
+                          genitive            = root + s("een"),
+                          genitives_plural    = [root + s("eiden"),
+                                                root + s("eitten")],
+                          partitives          = [root + s("utta")],
+                          partitives_plural   = [root + s("eita")],
+                          accusatives         = [word,
+                                                root + s("een")],
+                          accusative_plural   = root + s("eet"),
+                          inessive            = root + s("eessa"),
+                          inessives_plural    = [root + s("eissa")],
+                          elative             = root + s("eesta"),
+                          elatives_plural     = [root + s("eista")],
+                          illatives           = [root + s("eeseen")],
+                          illatives_plural    = [root + s("eisiin"),
+                                                 root + s("eihin")],
+                          adessive            = root + s("eella"),
+                          adessives_plural    = [root + s("eilla")],
+                          ablative            = root + s("eelta"),
+                          ablatives_plural    = [root + s("eilta")],
+                          allative            = root + s("eelle"),
+                          allatives_plural    = [root + s("eille")],
+                          essive              = root + s("eena"),
+                          essives_plural      = [root + s("eina")],
+                          translative         = root + s("eeksi"),
+                          translatives_plural = [root + s("eiksi")],
+                          abessive            = root + s("eetta"),
+                          abessives_plural    = [root + s("eitta")],
+                          instructives_plural = [root + s("ein")],
+                          comitatives_plural  = [root + s("eine")])
---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_47_kuollut.py
@@ -0,0 +1,83 @@
+import unittest
+from ensure import ensure
+from ..noun_inflection_info import InflectionInfo
+from .test_utils import ensure_inflections_equal
+from ..gradation import gradate_kotus_c_tyttö_tytön_kate_katteen
+from .kotus_noun_47_kuollut import kotus_noun_47_kuollut
+
+class TestInflection(unittest.TestCase):
+    def test_basic_example(self):
+        data = kotus_noun_47_kuollut("kuollut")
+
+        expected = InflectionInfo(nominative="kuollut",
+                                  nominative_plural   = "kuolleet",
+                                  genitive            = "kuolleen",
+                                  genitives_plural    = ["kuolleiden",
+                                                         "kuolleitten"],
+                                  partitives          = ["kuollutta"],
+                                  partitives_plural   = ["kuolleita"],
+                                  accusatives         = ["kuollut", "kuolleen"],
+                                  accusative_plural   = "kuolleet",
+                                  inessive            = "kuolleessa",
+                                  inessives_plural    = ["kuolleissa"],
+                                  elative             = "kuolleesta",
+                                  elatives_plural     = ["kuolleista"],
+                                  illatives           = ["kuolleeseen"],
+                                  illatives_plural    = ["kuolleisiin", "kuolleihin"],
+                                  adessive            = "kuolleella",
+                                  adessives_plural    = ["kuolleilla"],
+                                  ablative            = "kuolleelta",
+                                  ablatives_plural    = ["kuolleilta"],
+                                  allative            = "kuolleelle",
+                                  allatives_plural    = ["kuolleille"],
+                                  essive              = "kuolleena",
+                                  essives_plural      = ["kuolleina"],
+                                  translative         = "kuolleeksi",
+                                  translatives_plural = ["kuolleiksi"],
+                                  abessive            = "kuolleetta",
+                                  abessives_plural    = ["kuolleitta"],
+                                  instructives_plural = ["kuollein"],
+                                  comitatives_plural  = ["kuolleine"])
+
+        ensure_inflections_equal(expected, data)
+
+    def test_other_vowel_harmony_group(self):
+        # this doesn't exist in the word list, but it's a real and common word
+        data = kotus_noun_47_kuollut("nähnyt")
+        print(data)
+
+        expected = InflectionInfo(nominative='nähnyt',
+                                  nominative_plural='nähneet',
+                                  genitive='nähneen',
+                                  genitives_plural=['nähneiden',
+                                                    'nähneitten'],
+                                  partitives=['nähnyttä'],
+                                  partitives_plural=['nähneitä'],
+                                  accusatives=['nähnyt',
+                                               'nähneen'],
+                                  accusative_plural='nähneet',
+                                  inessive='nähneessä',
+                                  inessives_plural=['nähneissä'],
+                                  elative='nähneestä',
+                                  elatives_plural=['nähneistä'],
+                                  illatives=['nähneeseen'],
+                                  illatives_plural=['nähneisiin',
+                                                    'nähneihin'],
+                                  adessive='nähneellä',
+                                  adessives_plural=['nähneillä'],
+                                  ablative='nähneeltä',
+                                  ablatives_plural=['nähneiltä'],
+                                  allative='nähneelle',
+                                  allatives_plural=['nähneille'],
+                                  essive='nähneenä',
+                                  essives_plural=['nähneinä'],
+                                  translative='nähneeksi',
+                                  translatives_plural=['nähneiksi'],
+                                  abessive='nähneettä',
+                                  abessives_plural=['nähneittä'],
+                                  instructives_plural=['nähnein'],
+                                  comitatives_plural=['nähneine'])
+
+        ensure_inflections_equal(expected, data)
+
+    # all words use no gradation
