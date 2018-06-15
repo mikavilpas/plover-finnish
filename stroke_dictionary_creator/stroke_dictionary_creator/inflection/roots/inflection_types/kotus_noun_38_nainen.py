1a7dc6c6d26c3997fdb5146a369591ef21fadcd2
Mika Vilpas <mika.vilpas@gmail.com>
2018-06-15 18:10:15 +0300

    Add noun group 38 (nainen, hännällinen)

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/kotus_noun_38_nainen.py
@@ -0,0 +1,51 @@
+from .base import *
+
+from parsy import *
+from ..tokens import *
+
+@generate
+def word_nen_ending():
+    nen = yield string("nen")
+    root = yield character.at_least(1).concat()
+    return [root, nen]
+
+def kotus_noun_38_nainen(word, gradation_fn = identity):
+    # suffix
+    def s(text): return change_to_same_vowel_group_prefer_umlauts(word, text)
+
+    word_alt = gradation_fn(word)
+    (root, nen_ending) = reverse_parse(word, word_nen_ending)
+
+    # TODO the essive can also be "naisna" as well as "naisina", but this form
+    # is rare
+
+    return InflectionInfo(nominative          = word,
+                          nominative_plural   = root + s("set"),
+                          genitive            = root + s("sen"),
+                          genitives_plural    = [root + s("sten"),
+                                                root + s("sien")],
+                          partitives          = [root + s("sta")],
+                          partitives_plural   = [root + s("sia")],
+                          accusatives         = [word,
+                                                root + s("sen")],
+                          accusative_plural   = root + s("set"),
+                          inessive            = root + s("sessa"),
+                          inessives_plural    = [root + s("sissa")],
+                          elative             = root + s("sesta"),
+                          elatives_plural     = [root + s("sista")],
+                          illatives           = [root + s("seen")],
+                          illatives_plural    = [root + s("siin")],
+                          adessive            = root + s("sella"),
+                          adessives_plural    = [root + s("silla")],
+                          ablative            = root + s("selta"),
+                          ablatives_plural    = [root + s("silta")],
+                          allative            = root + s("selle"),
+                          allatives_plural    = [root + s("sille")],
+                          essive              = root + s("sena"),
+                          essives_plural      = [root + s("sina")],
+                          translative         = root + s("seksi"),
+                          translatives_plural = [root + s("siksi")],
+                          abessive            = root + s("setta"),
+                          abessives_plural    = [root + s("sitta")],
+                          instructives_plural = [root + s("sin")],
+                          comitatives_plural  = [root + s("sine")])
---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/test_kotus_noun_38_nainen.py
@@ -0,0 +1,80 @@
+import unittest
+from ensure import ensure
+from ..noun_inflection_info import InflectionInfo
+from .test_utils import ensure_inflections_equal
+from ..gradation import gradate_kotus_c_tyttö_tytön_kate_katteen
+from .kotus_noun_38_nainen import kotus_noun_38_nainen
+
+class TestInflection(unittest.TestCase):
+    def test_basic_example(self):
+        data = kotus_noun_38_nainen("nainen")
+
+        expected = InflectionInfo(nominative          = "nainen",
+                                  nominative_plural   = "naiset",
+                                  genitive            = "naisen",
+                                  genitives_plural    = ["naisten",
+                                                         "naisien"],
+                                  partitives          = ["naista"],
+                                  partitives_plural   = ["naisia"],
+                                  accusatives         = ["nainen", "naisen"],
+                                  accusative_plural   = "naiset",
+                                  inessive            = "naisessa",
+                                  inessives_plural    = ["naisissa"],
+                                  elative             = "naisesta",
+                                  elatives_plural     = ["naisista"],
+                                  illatives           = ["naiseen"],
+                                  illatives_plural    = ["naisiin"],
+                                  adessive            = "naisella",
+                                  adessives_plural    = ["naisilla"],
+                                  ablative            = "naiselta",
+                                  ablatives_plural    = ["naisilta"],
+                                  allative            = "naiselle",
+                                  allatives_plural    = ["naisille"],
+                                  essive              = "naisena",
+                                  essives_plural      = ["naisina"],
+                                  translative         = "naiseksi",
+                                  translatives_plural = ["naisiksi"],
+                                  abessive            = "naisetta",
+                                  abessives_plural    = ["naisitta"],
+                                  instructives_plural = ["naisin"],
+                                  comitatives_plural  = ["naisine"])
+
+        ensure_inflections_equal(expected, data)
+
+    def test_other_vowel_harmony_group(self):
+        data = kotus_noun_38_nainen("hännällinen")
+
+        expected = InflectionInfo(nominative='hännällinen',
+                                  nominative_plural='hännälliset',
+                                  genitive='hännällisen',
+                                  genitives_plural=['hännällisten',
+                                                    'hännällisien'],
+                                  partitives=['hännällistä'],
+                                  partitives_plural=['hännällisiä'],
+                                  accusatives=['hännällinen',
+                                               'hännällisen'],
+                                  accusative_plural='hännälliset',
+                                  inessive='hännällisessä',
+                                  inessives_plural=['hännällisissä'],
+                                  elative='hännällisestä',
+                                  elatives_plural=['hännällisistä'],
+                                  illatives=['hännälliseen'],
+                                  illatives_plural=['hännällisiin'],
+                                  adessive='hännällisellä',
+                                  adessives_plural=['hännällisillä'],
+                                  ablative='hännälliseltä',
+                                  ablatives_plural=['hännällisiltä'],
+                                  allative='hännälliselle',
+                                  allatives_plural=['hännällisille'],
+                                  essive='hännällisenä',
+                                  essives_plural=['hännällisinä'],
+                                  translative='hännälliseksi',
+                                  translatives_plural=['hännällisiksi'],
+                                  abessive='hännällisettä',
+                                  abessives_plural=['hännällisittä'],
+                                  instructives_plural=['hännällisin'],
+                                  comitatives_plural=['hännällisine'])
+
+        ensure_inflections_equal(expected, data)
+
+    # all words use no gradation
