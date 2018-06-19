bbdc8a19dfcede0b19922a897c86c7000284daf6
Mika Vilpas <mika.vilpas@gmail.com>
2018-06-19 13:24:40 +0300

    Fix some imports

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/inflection_service.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/inflection_service.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import stroke_dictionary_creator.inflection.voikko_inflection as voikko_inflection
-from stroke_dictionary_creator.inflection.parsing import word_and_class
+from . import voikko_inflection
+from .parsing import word_and_class
 
 def inflected_forms(word_and_infclass):
     (word, infclass) = word_and_class.parse(word_and_infclass)
