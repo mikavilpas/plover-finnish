175926ed8101afded9f9a76e80bcd9cc0261669e
Mika Vilpas <mika.vilpas@gmail.com>
2019-02-13 11:30:27 +0200

    Start redoing gradation to Joukahainen format

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/gradation.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/gradation.py
@@ -1,85 +1,14 @@
 from parsy import *
 from .parse_utils import reverse, reverse_parse
 from .tokens import *
-from toolz import itertoolz
+from collections import OrderedDict
+# from voikko import
 
-def find(word, substring):
-    if substring is None:
-        return None
-    index = word.find(substring)
-    return index if index >= 0 else None
-
-def is_substring_before(word, a, b):
-    pos_a = find(word, a)
-    pos_b = find(word, b)
-
-    if pos_a is None:
-        if pos_b is None:
-            # the applied gradation class is likely invalid by programmer mistake
-            raise Exception("cannot gradate {} from {} to {}. Neither found in word."\
-                            .format(word, a, b))
-        return False
-    elif pos_b is None:
-        return True
-    else: # both substrings found in word
-        return pos_a <= pos_b
-
-identity = lambda a: a
-
-def gradate(word, a, b):
-    word = reverse(word)
-    target = reverse(a)
-    replacement = reverse(b) if b else None
-
-    if is_substring_before(word, target, replacement):
-        word = word.replace(target, replacement or '', 1)
-        return reverse(word)
-    return None # unsuccessful, caller should try the other way
-
-def gradator(strong, weak):
-    def do_gradate(word):
-        return gradate(word, strong, weak) \
-            or gradate(word, weak, strong)
-    return do_gradate
-
-def one_way_gradator(strong, weak):
-    def do_gradate(word):
-        # This gradation class only works one way!
-        result = gradate(word, strong, weak)
-        if result is None:
-            raise Exception("cannot gradate {} from {} to {}."
-                            .format(word, strong, weak))
-        return result
-
-    return do_gradate
-
-gradate_kotus_a_takki_takin_hake_hakkeen  = gradator("kk", "k")
-gradate_kotus_b_kaappi_kaapin_opas_oppaan = gradator("pp", "p")
-gradate_kotus_c_tyttö_tytön_kate_katteen  = gradator("tt", "t")
-
-# gradation d is defined below
-
-gradate_kotus_e_sopu_sovun_taive_taipeen        = gradator("p", "v")
-gradate_kotus_f_satu_sadun_keidas_keitaan       = gradator("t", "d")
-gradate_kotus_g_aurinko_auringon_rengas_renkaan = gradator("nk", "ng")
-gradate_kotus_h_kumpi_kumman_lumme_lumpeen      = gradator("mp", "mm")
-gradate_kotus_i_ilta_illan_sivellin_siveltimen  = gradator("lt", "ll")
-gradate_kotus_j_hento_hennon_vanne_vanteen      = gradator("nt", "nn")
-gradate_kotus_k_virta_virran_porras_portaan     = gradator("rt", "rr")
-gradate_kotus_l_arki_arjen_hylje_hylkeen        = gradator("k", "j")
-gradate_kotus_m_suku_suvun                      = one_way_gradator("k", "v")
-
-
-#### Gradation d is more complex, defined below.
-# Joukahainen splits the gradation group d into two groups. Here are their
-# meanings:
+# Definitions for the more machine readable Joukahainen mappings into the
+# (could be argued) more understandable Kotus system for gradation classes.
 #
-# | Joukahainen | Kotus            | Example    |
-# | av5         | group d, k -> '' | laki-lait  |
-# | av6         | group d, '' -> k | aie-aikeet |
-#
-
-gradate_kotus_d_joukahainen_av5_reikä_reiän = one_way_gradator("k", None)
+# The source for (the implementation of) these mappings can be found in
+# https://github.com/sp3ctum/corevoikko/blob/feature%2Fexploration/voikko-fi/common/hfconv.py#L23
 
 def gradate_kotus_d_joukahainen_av6_aie_aikeen(word) -> str:
     @generate
@@ -92,3 +21,138 @@ def gradate_kotus_d_joukahainen_av6_aie_aikeen(word) -> str:
         return end + vowel2 + "k" + vowel1 + start
 
     return reverse(split_double_vowel_with_k.parse(reverse(word)))
+
+def gradate(word, a, b):
+    word = reverse(word)
+    target = reverse(a)
+    replacement = reverse(b) if b else None
+
+    word = word.replace(target, replacement or '', 1)
+    return reverse(word)
+
+def gradator(from_, to):
+    def do_gradate(word):
+        result = gradate(word, from_, to)
+        if result is None:
+            raise Exception("cannot gradate {} from {} to {}."
+                            .format(word, from_, to))
+        return result
+
+    return do_gradate
+
+def find(word, substring):
+    if substring is None:
+        return None
+    index = word.find(substring)
+    return index if index >= 0 else None
+
+def compile_voikko_gradation_regexp(pattern: str):
+    import re
+    # copied from hfconv
+    pattern = pattern.replace('V', '(?:a|á|e|i|o|u|y|ä|ö|é)')
+    pattern = pattern.replace('C', '(?:b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z|š|ž)')
+    pattern = pattern.replace('A', '(?:a|ä)')
+    pattern = pattern.replace('O', '(?:o|ö)')
+    pattern = pattern.replace('U', '(?:u|y)')
+    regExp = re.compile('^' + pattern + '$', re.IGNORECASE)
+    return regExp
+
+def compile_gradation_regex(gradclass):
+    # gradclass:
+    # ('valo', 'sw', [gradations])
+    (refword, grade, gradations) = gradclass
+
+    # gradations:
+    # [('nt','(.*n)tU','lintu')]
+    new_gradations = ((gradtype,
+                       compile_voikko_gradation_regexp(pattern),
+                       *rest)
+                      for (gradtype, pattern, *rest)
+                      in gradations)
+
+    return (refword, grade, new_gradations)
+
+# Inflection class map
+from .voikko_hacks import hfconv
+CLASSMAP = map(compile_gradation_regex, hfconv.modern_classmap)
+
+def gradate_joukahainen(word, refword, gradation_class):
+    gradations = reference_options(word, refword)
+    matching = matching_gradations(word, gradation_class, gradations)
+    gradtype = matching[0]
+
+    print(gradation_class)
+    print(gradtype)
+    gradator_fn = gradation_dispatch[gradation_class][gradtype]
+    gradated_word = gradator_fn(word)
+
+    return gradated_word
+
+def matching_gradations(word, gradation_class: str, gradations):
+    # Gradations are allowed based on the gradation_class, for example for the
+    # words "lobata" (av2) and "taata" (av6), there could be false positives if
+    # this weren't taken into account.
+    allowed = allowed_gradations(gradation_class, gradations)
+    matching = (grad_type
+                for (grad_type, grad_regex, *_) in gradations
+                if grad_regex.match(word)
+                and grad_type in allowed)
+
+    def _gradation_length(gradation_type):
+        # eg. k>, kk, uku, lt, nt, tt, nk, ...
+        return 0 if gradation_type is None else len(gradation_type)
+
+    return sorted(matching, key = _gradation_length, reverse=True)
+
+gradation_dispatch = dict(
+    # the gradations have the same keys as defined in hfconv
+    av1 = dict(
+        # strong to weak
+        uku = gradator("uku", "uvu"), # kotus M
+        yky = gradator("yky", "yvy"), # kotus M (again)
+        tt  = gradator("tt",  "t"),   # kotus C
+        pp  = gradator("pp",  "p"),   # kotus B
+        kk  = gradator("kk",  "k"),   # kotus A
+        mp  = gradator("mp",  "mm"),  # kotus H
+        nt  = gradator("nt",  "nn"),  # kotus J
+        lt  = gradator("lt",  "ll"),  # kotus I
+        rt  = gradator("rt",  "rr"),  # kotus K
+        nk  = gradator("nk",  "ng"),  # kotus G
+        p   = gradator("p",   "v"),   # kotus E
+        t   = gradator("t",   "d"),   # kotus F
+    ),
+    av2 = dict(
+        # weak to strong
+        mm = gradator("mm", "mp"), # kotus H
+        nn = gradator("nn", "nt"), # kotus J
+        ll = gradator("ll", "lt"), # kotus I
+        rr = gradator("rr", "rt"), # kotus K
+        ng = gradator("ng", "nk"), # kotus G
+        b = gradator("b",  "bb"),  # kind of like kotus B. (lobata -> lobbaan)
+        g = gradator("g",  "gg"),  # kind of like kotus B. (digata -> diggaan)
+        t = gradator("t",  "tt"),  # kotus C
+        p = gradator("p",  "pp"),  # kotus B
+        k = gradator("k",  "kk"),  # kotus A
+        v = gradator("v",  "p"),   # kotus E
+        d = gradator("d",  "t"),   # kotus F
+    ),
+    av3 = {"k>j": gradator("k", "j")}, # kotus L, strong to weak
+    av4 = {"j>k": gradator("j", "k")}, # kotus L, weak to strong
+    av5 = {"k>" : gradator("k", "")},  # kotus D, strong to weak
+
+    # kotus D, weak (nonexisting) to strong
+    av6 = {">k" : gradate_kotus_d_joukahainen_av6_aie_aikeen}
+)
+
+def allowed_gradations(gradation_class: str, gradations):
+    return [grad_type
+            for (grade, grad_type, grad_class) in hfconv.grads
+            if grad_class == gradation_class]
+
+def reference_options(word, refword):
+    print([options
+           for (classname, grade, options) in CLASSMAP
+           if classname == refword ])
+    return next(options
+                for (classname, grade, options) in CLASSMAP
+                if classname == refword)
--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/test_gradation.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/test_gradation.py
@@ -3,88 +3,42 @@ from ensure import ensure
 from . import gradation as g
 
 class TestGradation(unittest.TestCase):
-    def test_gradate_kotus_a_takki_takin_hake_hakkeen(self):
-        gradate = g.gradate_kotus_a_takki_takin_hake_hakkeen
-        ensure(gradate("takki")).equals("taki")
-        ensure(gradate("hake")).equals("hakke")
+    def test_gradate_joukahainen_av1(self):
+        gradate = g.gradate_joukahainen
 
-    def test_gradate_kotus_b_kaappi_kaapin_opas_oppaan(self):
-        gradate = g.gradate_kotus_b_kaappi_kaapin_opas_oppaan
-        ensure(gradate("kaappi")).equals("kaapi")
-        ensure(gradate("opas")).equals("oppas")
+        # try each gradation
+        ensure(gradate("lobata", "salata", "av2")).equals("lobbata")
+        ensure(gradate("taata", "salata", "av6")).equals("takata")
+    #     ensure(gradate("alppi")).equals("alpi")
+    #     ensure(gradate("aavikko")).equals("aaviko")
+    #     ensure(gradate("ampua")).equals("ammua")
+    #     ensure(gradate("apu")).equals("avu")
+    #     ensure(gradate("antaa")).equals("annaa")
+    #     ensure(gradate("aalto")).equals("aallo")
+    #     ensure(gradate("juurtaa")).equals("juurraa")
+    #     ensure(gradate("ahtaa")).equals("ahdaa")
+    #     ensure(gradate("hanki")).equals("hangi")
+    #     ensure(gradate("suku")).equals("suvu")
+    #     ensure(gradate("kyky")).equals("kyvy")
 
-    def test_gradate_kotus_c(self):
-        gradate = g.gradate_kotus_c_tyttö_tytön_kate_katteen
-        ensure(gradate("tyttö")).equals("tytö")
-        ensure(gradate("kate")).equals("katte")
-        ensure(gradate("esittämätön")).equals("esittämättön")
+    # def test_gradate_joukahainen_av2(self):
+    #     gradate = g.gradate_joukahainen_av2
 
-    def test_gradate_kotus_e_sopu_sovun_taive_taipeen(self):
-        gradate = g.gradate_kotus_e_sopu_sovun_taive_taipeen
-        ensure(gradate("sopu")).equals("sovu")
-        ensure(gradate("taive")).equals("taipe")
-        ensure(gradate("taipe")).equals("taive")
-
-    def test_gradate_kotus_f_satu_sadun_keidas_keitaan(self):
-        gradate = g.gradate_kotus_f_satu_sadun_keidas_keitaan
-        ensure(gradate("satu")).equals("sadu")
-        ensure(gradate("keidas")).equals("keitas")
-
-    def test_gradate_kotus_g_aurinko_auringon_rengas_renkaan(self):
-        gradate = g.gradate_kotus_g_aurinko_auringon_rengas_renkaan
-        ensure(gradate("aurinko")).equals("auringo")
-        ensure(gradate("rengas")).equals("renkas")
-
-    def test_gradate_kotus_h_kumpi_kumman_lumme_lumpeen(self):
-        gradate = g.gradate_kotus_h_kumpi_kumman_lumme_lumpeen
-        ensure(gradate("kumpi")).equals("kummi")
-        ensure(gradate("lumme")).equals("lumpe")
-
-    def test_gradate_kotus_i_ilta_illan_sivellin_siveltimen(self):
-        gradate = g.gradate_kotus_i_ilta_illan_sivellin_siveltimen
-        ensure(gradate("ilta")).equals("illa")
-        ensure(gradate("sivellin")).equals("siveltin")
-
-    def test_gradate_kotus_j_hento_hennon_vanne_vanteen(self):
-        gradate = g.gradate_kotus_j_hento_hennon_vanne_vanteen
-        ensure(gradate("hento")).equals("henno")
-        ensure(gradate("vanne")).equals("vante")
-        ensure(gradate("käpälöinti")).equals("käpälöinni")
-
-    def test_gradate_kotus_k_virta_virran_porras_portaan(self):
-        gradate = g.gradate_kotus_k_virta_virran_porras_portaan
-        ensure(gradate("virta")).equals("virra")
-        ensure(gradate("porras")).equals("portas")
-
-    def test_gradate_kotus_l_arki_arjen_hylje_hylkeen(self):
-        gradate = g.gradate_kotus_l_arki_arjen_hylje_hylkeen
-        ensure(gradate("arki")).equals("arji")
-        ensure(gradate("hylje")).equals("hylke")
-
-    def test_gradate_kotus_gradate_kotus_m_suku_suvun(self):
-        gradate = g.gradate_kotus_m_suku_suvun
-        ensure(gradate("suku")).equals("suvu")
-        ensure(gradate).called_with("suvu").raises(Exception) # one way gradation
-
-    def test_gradate_kotus_d_joukahainen_av5(self):
-        gradate = g.gradate_kotus_d_joukahainen_av5_reikä_reiän
-        # halkoa
-        ensure(gradate("aikoa")).equals("aioa")
-        ensure(gradate("halkoa")).equals("haloa")
-        ensure(gradate("hauki")).equals("haui")
-        ensure(gradate("hiki")).equals("hii")
-
-    def test_gradate_kotus_d(self):
-        gradate = g.gradate_kotus_d_joukahainen_av6_aie_aikeen
-        ensure(gradate("ien")).equals("iken")
-        ensure(gradate("aie")).equals("aike")
-        ensure(gradate("maata")).equals("makata")
-        ensure(gradate("aueta")).equals("auketa")
-        ensure(gradate("aueta")).equals("auketa")
-
-        ensure(gradate("reiä")).equals("reikä")
-        ensure(gradate("lai")).equals("laki")
-        ensure(gradate("hae")).equals("hake")
-        ensure(gradate("hiota")).equals("hikota")
-        ensure(gradate("koe")).equals("koke")
-        ensure(gradate("liietä")).equals("liiketä")
+    #     # ('b',  'bb'), # kind of like kotus B. (lobata -> lobbaan)
+    #     ensure(gradate("lobata")).equals("lobbata")
+    #     # ('g',  'gg'), # kind of like kotus B. (digata -> diggaan)
+    #     ensure(gradate("digata")).equals("diggaan")
+    #     # ('t',  'tt'), # kotus C
+    #     ensure(gradate("aate")).equals("aatte")
+    #     # ('p',  'pp'), # kotus B
+    #     ensure(gradate("lape")).equals("lappe")
+    #     # ('k',  'kk'), # kotus A
+    #     ensure(gradate("aluke")).equals("alukke")
+    #     # ('mm', 'mp'), # kotus H
+    #     # ('v',  'p'),  # kotus E
+    #     # ('nn', 'nt'), # kotus J
+    #     # ('ll', 'lt'), # kotus I
+    #     # ('rr', 'rt'), # kotus K
+    #     # ('d',  't'),  # kotus F
+    #     # ('ng', 'nk'), # kotus G
+    #     pass
