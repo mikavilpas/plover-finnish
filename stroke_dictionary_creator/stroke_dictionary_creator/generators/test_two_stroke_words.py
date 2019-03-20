8f8a999020ace9cd7cf824205b17764433a7226b
Mika Vilpas <mika.vilpas@gmail.com>
2019-03-20 14:28:08 +0200

    Add some tests to illustrate strokefication

--- stroke_dictionary_creator/stroke_dictionary_creator/generators/test_two_stroke_words.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/generators/test_two_stroke_words.py
@@ -10,3 +10,11 @@ class TestTwoStrokeWords(unittest.TestCase):
     def test_with_suffix(self):
         ensure(two_strokes("kokon")).equals(["koko", "KOKo"])
         ensure(two_strokes("aamuiksi")).equals(["aamui", "A*SHeoi"])
+
+        ensure(two_strokes("aattoja")).equals(["aattoja", "A*STRoia"])
+
+        ensure(two_strokes("aatelit")).equals(["aatelit", "A*Te/HRIT"])
+        ensure(two_strokes("aatelistoa")).equals(["aatelistoa", "A*Te/HRISToa"])
+        ensure(two_strokes("aatelistoja")).equals(["aatelistoja", "A*Te/HRISToia"])
+
+        ensure(two_strokes("etenen")).equals(["etenen", "ETe/EN"])
