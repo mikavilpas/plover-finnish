31bcfa82b0b55b40cde202a8ef34f01151b54d37
Mika Vilpas <mika.vilpas@gmail.com>
2019-04-08 11:24:03 +0300

    whitespace

--- stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/adjectives/joukahainen_adjective_iäkäs.py
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/adjectives/joukahainen_adjective_iäkäs.py
@@ -6,8 +6,8 @@ def joukahainen_adjective_iäkäs(word, gradation_fn = base.identity) -> Adjecti
 
     # all words in this group are av2 and end with "kas"/"käs"
     (hapokk, a, s) = base.reverse_parse(gradation_fn(word), base.root_and_vc_ending)
-    hapokkaampi  = hapokk + a + a + "mpi"
-    hapokkain    = hapokk + a + "in"
+    hapokkaampi    = hapokk + a + a + "mpi"
+    hapokkain      = hapokk + a + "in"
 
     return Adjective(positive    = inflect_with(word, "iäkäs", gradation_fn),
                      comparative = inflect_as(hapokkaampi, "suurempi"),
