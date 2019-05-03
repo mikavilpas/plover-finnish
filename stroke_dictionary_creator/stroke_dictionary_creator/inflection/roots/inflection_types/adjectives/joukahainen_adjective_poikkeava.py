c75d9e3ed176dce6dbf780de83ed35b92adb442f
Mika Vilpas <mika.vilpas@gmail.com>
2019-05-03 10:57:54 +0300

    Add stub for joukahainen_adjective_poikkeava

---
+++ stroke_dictionary_creator/stroke_dictionary_creator/inflection/roots/inflection_types/adjectives/joukahainen_adjective_poikkeava.py
@@ -0,0 +1,6 @@
+from .. import base
+from .adjective import Adjective, inflect_as, inflect_with
+
+
+def joukahainen_adjective_poikkeava(word, gradation_fn = base.identity) -> Adjective:
+    raise Exception("not implemented yet")
